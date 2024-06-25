9fe77dcec5080848e4f791f1eb3eea37ab126ec4
Heighties <heighties@gmail.com>
2024-06-26 00:28:08 +0200

    contact title size

--- src/sections/Contact/Contact.jsx
+++ src/sections/Contact/Contact.jsx
@@ -13,25 +13,25 @@ const Contact = () => {
             <div className='flex flex-col w-3/4 xl:w-1/2 gap-2'>
               <div className='flex flex-row gap-4'>
                 <div className="w-1/2">
-                  <label htmlFor="firstName" className="block text-white font-semibold mb-1">Prénom</label>
+                  <label htmlFor="firstName" className="block text-white text-sm font-semibold mb-1">Prénom</label>
                   <input type="text" id="firstName" name="firstName" className="w-full border border-gray-300 rounded-md xl:py-2 px-3" />
                 </div>
                 <div className="w-1/2">
-                  <label htmlFor="lastName" className="block text-white font-semibold mb-1">Nom</label>
+                  <label htmlFor="lastName" className="block text-white text-sm font-semibold mb-1">Nom</label>
                   <input type="text" id="lastName" name="lastName" className="w-full border border-gray-300 rounded-md xl:py-2 px-3" />
                 </div>
               </div>
               <div className="w-full">
-                <label htmlFor="email" className="block text-white font-semibold mb-1">E-mail</label>
+                <label htmlFor="email" className="block text-white text-sm font-semibold mb-1">E-mail</label>
                 <input type="email" id="email" name="email" className="w-full border border-gray-300 rounded-md xl:py-2 px-3" />
               </div>
               <div className="w-full">
-                <label htmlFor="subject" className="block text-white font-semibold mb-1">Objet</label>
+                <label htmlFor="subject" className="block text-white text-sm font-semibold mb-1">Objet</label>
                 <input type="text" id="subject" name="subject" className="w-full border border-gray-300 rounded-md xl:py-2 px-3" />
               </div>
             </div>
             <div className="w-3/4 h-32 xl:w-1/2 xl:h-[198px]">
-              <label htmlFor="message" className="block text-white font-semibold mb-1">Votre message</label>
+              <label htmlFor="message" className="block text-white text-sm font-semibold mb-1">Votre message</label>
               <textarea id="message" name="message" className="w-full h-full border border-gray-300 rounded-md resize-none" rows="10"></textarea>
             </div>
           </div>
