f8c2a5a50b3f379ad67138bab1a2f52a6670fb5f
Heighties <heighties@gmail.com>
2024-06-26 14:19:00 +0200

    MobileMenu responsive

--- src/components/MobileMenu/MobileMenu.jsx
+++ src/components/MobileMenu/MobileMenu.jsx
@@ -7,7 +7,7 @@ const MobileMenu = ({ scrollToSection, setMobileMenuOpen }) => {
   };
 
   return (
-    <div className="fixed top-0 left-0 w-full h-full bg-slate-900 bg-opacity-95 text-white flex flex-col gap-8 items-center justify-center z-40">
+    <div className="fixed top-0 left-0 w-full h-full bg-slate-900 bg-opacity-95 text-white flex flex-col gap-8 items-center justify-center z-20">
       <button className="text-3xl my-4" onClick={() => handleSectionClick("aboutme")}>Infos</button>
       <button className="text-3xl my-4" onClick={() => handleSectionClick("skills")}>Compétences</button>
       <button className="text-3xl my-4" onClick={() => handleSectionClick("portfolio")}>Portfolio</button>
