a40b70a82948161fb1c37fbfe89e8814e8fb97a5
Heighties <heighties@gmail.com>
2024-06-26 19:56:15 +0200

    contact responsive

--- src/sections/Contact/Contact.jsx
+++ src/sections/Contact/Contact.jsx
@@ -3,8 +3,8 @@ import EndCard from '../../components/Card3D/EndCard';
 
 const Contact = () => {
   return (
-    <div className="flex flex-col items-center w-full h-full mt-8 xl:mt-52 relative [perspective:1000px]">
-      <h2 className="font-bold text-white text-2xl xl:text-4xl">Me contacter</h2>
+    <div className="flex flex-col items-center w-full h-full xl:mt-52 relative [perspective:1000px]">
+      <h2 className="font-bold text-white text-2xl mt-8 xl:mt-0 xl:text-4xl">Me contacter</h2>
       <div className="xl:p-8 mt-8 xl:mt-0 rounded-lg xl:w-[80%]">
         
         {/* Formulaire de contact */}
