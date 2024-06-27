856e1e71d291bcd750eb065dd67e773c499be069
Heighties <heighties@gmail.com>
2024-06-27 10:21:21 +0200

    margin-top title

--- src/sections/Contact/Contact.jsx
+++ src/sections/Contact/Contact.jsx
@@ -33,8 +33,14 @@ const Contact = () => {
     }
   };
 
+  const handleFocus = (event) => {
+    const element = event.target;
+    const y = element.getBoundingClientRect().top + window.scrollY;
+    window.scrollTo({ top: y - 50, behavior: 'smooth' });
+  };
+
   return (
-    <div className="flex flex-col items-center w-full h-full xl:mt-52 relative [perspective:1000px]">
+    <div className="flex flex-col items-center w-full min-h-screen xl:mt-52 mb-16 relative [perspective:1000px] contact-section">
       <h2 className="font-bold text-white text-2xl mt-8 xl:mt-0 xl:text-4xl">Me contacter</h2>
       <div className="xl:p-8 mt-8 xl:mt-0 rounded-lg xl:w-[80%]">
         
@@ -45,25 +51,25 @@ const Contact = () => {
               <div className='flex flex-row gap-4'>
                 <div className="w-1/2">
                   <label htmlFor="firstName" className="block text-white text-sm font-semibold mb-1">Prénom</label>
-                  <input type="text" id="firstName" name="firstName" className="w-full border border-gray-300 rounded-md xl:py-2 px-3" onChange={handleChange} />
+                  <input type="text" id="firstName" name="firstName" className="w-full border border-gray-300 rounded-md xl:py-2 px-3" onChange={handleChange} onFocus={handleFocus} />
                 </div>
                 <div className="w-1/2">
                   <label htmlFor="lastName" className="block text-white text-sm font-semibold mb-1">Nom</label>
-                  <input type="text" id="lastName" name="lastName" className="w-full border border-gray-300 rounded-md xl:py-2 px-3" onChange={handleChange} />
+                  <input type="text" id="lastName" name="lastName" className="w-full border border-gray-300 rounded-md xl:py-2 px-3" onChange={handleChange} onFocus={handleFocus} />
                 </div>
               </div>
               <div className="w-full">
                 <label htmlFor="email" className="block text-white text-sm font-semibold mb-1">E-mail</label>
-                <input type="email" id="email" name="email" className="w-full border border-gray-300 rounded-md xl:py-2 px-3" onChange={handleChange} />
+                <input type="email" id="email" name="email" className="w-full border border-gray-300 rounded-md xl:py-2 px-3" onChange={handleChange} onFocus={handleFocus} />
               </div>
               <div className="w-full">
                 <label htmlFor="subject" className="block text-white text-sm font-semibold mb-1">Objet</label>
-                <input type="text" id="subject" name="subject" className="w-full border border-gray-300 rounded-md xl:py-2 px-3" onChange={handleChange} />
+                <input type="text" id="subject" name="subject" className="w-full border border-gray-300 rounded-md xl:py-2 px-3" onChange={handleChange} onFocus={handleFocus} />
               </div>
             </div>
             <div className="w-3/4 h-32 xl:w-1/2 xl:h-[198px]">
               <label htmlFor="message" className="block text-white text-sm font-semibold mb-1">Votre message</label>
-              <textarea id="message" name="message" className="w-full h-full border border-gray-300 rounded-md resize-none" rows="10" onChange={handleChange}></textarea>
+              <textarea id="message" name="message" className="w-full h-full border border-gray-300 rounded-md resize-none" rows="10" onChange={handleChange} onFocus={handleFocus}></textarea>
             </div>
           </div>
           <button type="submit" className="bg-slate-700 text-white border-2 border-white font-bold py-2 px-4 mt-10 xl:w-1/6 rounded-full">Envoyer</button>
