0c3f0fd3511612039f004ca394a0cbff9750bc95
Heighties <heighties@gmail.com>
2025-06-05 17:36:27 +0200

    test only light

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  darkMode: false,
   theme: {
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-          "parallax" : 'url("../public/technology.jpg")',
+        parallax: 'url("../public/technology.jpg")',
       },
-      aspectRatio : {
+      aspectRatio: {
         card: "0.71",
-        card2: "2"
+        card2: "2",
       },
-      borderRadius :{
+      borderRadius: {
         32: "2rem",
         "32-tl-bl": "2rem 0 0 2rem",
         "32-tr-br": "0 2rem 2rem 0",
         "32-tl-tr": "2rem 2rem 0 0",
-      }
+      },
     },
   },
   plugins: [],
-}
+};
